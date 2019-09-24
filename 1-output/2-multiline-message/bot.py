52234af3fa53019c3554befb04f080b501c1a7fc
4josew16 <4josew16@solent.ac.uk>
2019-09-24 09:47:02 +0000

    Added code to display a multi-line message

---
+++ 1-output/2-multiline-message/bot.py
@@ -0,0 +1,8 @@
+print("system failure Imminent")
+print ("system reboot has been initiated...")
+print ()
+print ("rebooting sensory system")
+print ("rebooting output motors")
+print ("rebooting hover engine")
+print ()
+print ("system online")
