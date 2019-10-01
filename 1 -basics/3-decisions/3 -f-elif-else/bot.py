57fdfead52faf6dd3bfdb0c1175524d462c2c7be
4josew16 <4josew16@solent.ac.uk>
2019-10-01 10:21:35 +0000

    Added code to demonstrate theh use of an if...elif...else statement.

---
+++ 1 -basics/3-decisions/3 -f-elif-else/bot.py
@@ -0,0 +1,14 @@
+Direction =input ("In what direction are you painting?")
+if Direction == "upwards":
+    print("I am painting in the upward direction")
+elif Direction =="downwards":
+    print("I am painting in the downward direction")
+
+elif Direction =="left":
+    print ("I am painting in the left direction")
+
+elif Direction =="right":
+     print ("I am painting in the right direction")
+
+else:
+    print ("You are not painting")
