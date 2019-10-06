d9967f20b6603a33449715c06b67d7d59d3c72e3
4josew16 <4josew16@solent.ac.uk>
2019-10-06 11:57:03 +0000

    Added code to display an ascii art robot

--- 1-output/4-ascii-art/bot.py
+++ 1-output/4-ascii-art/bot.py
@@ -1,8 +1,13 @@
-print ("please enter robot eye")
-eye = input ()
+#Display an ascii art robot
+print("##########")
+print("# o  0 #")
+print("# ---- #")
+print("##########")
 
-print ("##########")
-print ("# " + eye + "    " + eye + " #")
-print ("#        #")
-print ("#        #")
-print ("##########")
+#Display an scii art robot using a long string 
+print("""
+##########
+# o o #
+# --- #
+##########)
+""")
