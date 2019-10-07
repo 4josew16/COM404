a944c1335d7b673eceaae3fa3dda035b6ea3b3ab
4josew16 <4josew16@solent.ac.uk>
2019-10-07 19:28:36 +0000

    Added to demonstrate the use of comparison operators

---
+++ 1 -basics/3-decisions/5-comparison/bot.py
@@ -0,0 +1,6 @@
+print("Please enter the first number between 10 and 15")
+first_number =input()
+print("Please enter the second number which must be less than 10")
+second_number =input()
+if (second_number > first_number):
+    print("The second number is the smallest")
