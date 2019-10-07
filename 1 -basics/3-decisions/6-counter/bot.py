ed563c0a42080ac84e9d75642e582ad98cb33194
4josew16 <4josew16@solent.ac.uk>
2019-10-07 22:22:11 +0000

    added code to demonstrate how a variable can be used as a counter

---
+++ 1 -basics/3-decisions/6-counter/bot.py
@@ -0,0 +1,23 @@
+num_1 = int(input("Please enter the first whole number:"))
+mod_1 = num_1 % 2
+if mod_1 > 0:
+    print ("This is an odd number")
+else:
+    print("This is an even number")
+num_2 = int(input("Please enter the second number:"))
+mod_2 = num_2 % 2
+if mod_2 > 0:
+    print ("This is an odd number")
+else:
+    print("This is an even number")
+num_3 = int(input("Please enter the third_number:"))
+mod_3 = num_3 % 2
+if mod_3 > 0:
+    print ("This is an odd number")
+else:
+    print("This is an even number")
+Z=['This is an odd number','This is an even number']
+Number_count =(Z)
+print(Number_count)
+Number = ['This is an odd number','This is an even number']
+print(Z, Number_count[Z])
