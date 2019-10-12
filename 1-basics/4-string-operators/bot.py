9ae2d95c3efdb360eb16c268da4a9c15e0845fc8
4josew16 <4josew16@solent.ac.uk>
2019-10-12 11:55:09 +0000

    Created a basic input/output program.

---
+++ 1-basics/4-string-operators/bot.py
@@ -0,0 +1,12 @@
+# Get bot data
+print("Please enter number of lives")
+lives=int(input())
+print("Please enter the energy level")
+energy =int(input())
+print("Please enter the shield level")
+shield=int(input())
+print()
+#Display bot data
+print("Lives:","♥" * lives)
+print("Energy:", "♦" * energy)
+print("Shield:", "♦" * shield)
