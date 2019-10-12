20ea83719257d6a585f97b0c9735fb1ca9788764
4josew16 <4josew16@solent.ac.uk>
2019-10-12 17:52:56 +0000

    Added code to demonstrate the use of comparison operators.

---
+++ 1-basics/3-decision/1-simple-decision/5-comparison-operators/bot.py
@@ -0,0 +1,12 @@
+# get the user to enter a number 
+print("Please enter the first number (must be between 1 and 10)") # create variable for first number 
+first_number = int(input())
+# get user to enter a second number
+print("Please enter a second number (must be between 1 and 10)") # create a variable for the second number
+second_number=int(input())
+# create a comparison operator 
+if (first_number>=second_number):
+    print("The first number is the biggest")
+elif (second_number<=first_number):
+    print("The second number is the smallest")
+
