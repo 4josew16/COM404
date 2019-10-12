f9aa2238e505303d0e3fcb25c35dd4f25316e465
4josew16 <4josew16@solent.ac.uk>
2019-10-12 17:23:49 +0000

    Added code to demonstrate the use of a modulus operator.

---
+++ 1-basics/3-decision/1-simple-decision/4-modulo/bot.py
@@ -0,0 +1,8 @@
+# Display if number is odd or even 
+# Get user to input a whole number 
+print("Please enter a number")
+number=int (input())
+if (number %2==0):
+    print("\n The number is an even number")
+else:
+    print("\n The number is an odd number")
