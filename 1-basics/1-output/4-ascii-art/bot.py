ffc5486899050177cd824c5e7b237a91ee619fc8
4josew16 <4josew16@solent.ac.uk>
2019-10-13 11:32:45 +0000

    Added code to display an ascii robot

--- 1-basics/1-output/4-ascii-art/bot.py
+++ 1-basics/1-output/4-ascii-art/bot.py
@@ -10,6 +10,6 @@ eye=input()
 
 # Display an ascii art robot
 print("##########")
-print("#  "+ eye + " " +  eye + "   #")
-print("# -----  #") 
+print("#  " + eye +  "  "  + eye + "  #")
+print("#  ----  #") 
 print("##########")
