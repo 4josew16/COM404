8bc3fa08a136cc4415d685d488a6268da9d4aee6
4josew16 <4josew16@solent.ac.uk>
2019-10-14 21:11:31 +0000

    Added code to demonstrate the usr of a while loop in calculating the factorial of a number.

---
+++ 1-basics/4-repetition/1-while-loop/6-sum-user-number/bot.py
@@ -0,0 +1,10 @@
+# Find the sum of digits of a given number 
+print("How many numbers should I sum up?")
+answer =int(input())
+sum_1 = 0
+while sum_1+1 <=answer:
+    print("Please enter number" ((sum_1)))
+
+
+
+ 
---
+++ 1-basics/4-repetition/1-while-loop/7-factorial/bot.py
@@ -0,0 +1,4 @@
+print("Please enter a non negative number:")
+num_1-int(input()
+product = 1
+for i in range(num_1)
