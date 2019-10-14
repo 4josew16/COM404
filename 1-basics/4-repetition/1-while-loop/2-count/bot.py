e6d801e72fcef186c05554c0a6ea64d1b2a3745f
4josew16 <4josew16@solent.ac.uk>
2019-10-14 19:49:44 +0000

    Added code to demonstrate the use of a while loop with len function.

--- 1-basics/4-repetition/1-while-loop/2-count/bot.py
+++ 1-basics/4-repetition/1-while-loop/2-count/bot.py
@@ -1,6 +1,12 @@
+#this is there to ask how many cables shouldbe avoided 
 cable_num=int(input("How many live cables must I avoid? "))
+#this variable is there because it is the thing that will be incramented in the loop could be called anything
 counter=0
+#this  is the while loop which starts the looping in the program
 while counter+1 <= cable_num:
+    #this is the line that prints out the statement that is asked
     print("Avoiding.... Done!" + str((counter +1)) + "live cables have been avoided")
+    #this makes the counter go up by one
     counter +=1
+print()
 print("All live cables have been avoided")
---
+++ 1-basics/4-repetition/1-while-loop/4-len/bot.py
@@ -0,0 +1,5 @@
+# Create a program to communicate with Beep 
+print ("Please enter a phrase.")
+phrase = input()
+lengh_of_phrase = (len(phrase))
+print("Bop " * lengh_of_phrase)
