5c40e1a5fa9d9b1172effef899a104c1f6eb69cb
4josew16 <4josew16@solent.ac.uk>
2019-10-15 09:18:41 +0000

    Added code to demonstrate the use of a user-defined function.

---
+++ 1-basics/5-functions/2-nesting/bot.py
@@ -0,0 +1,9 @@
+def identify():
+    print("What do you see?")
+    response=input()
+    if response =="large boulder":
+        print("it's time to run")
+    else:
+        print("we will be fine")
+identify()
+
