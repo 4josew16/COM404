43f8c3196b5619e0e30cc80430180b112449fe37
4josew16 <4josew16@solent.ac.uk>
2019-10-19 13:09:58 +0000

    Added code to demonstrate the use of return values.

--- 1-basics/5-functions/7-return-values/bot.py
+++ 1-basics/5-functions/7-return-values/bot.py
@@ -1,36 +1,23 @@
 # using return values 
 # define bots characteristics 
-name_1 = "Beep"
-weight_1 = 12
-
-name_2 = "Bop"
-weight_2 = 20
-
-total_weight = (weight_1 + weight_2)
-average_weight = (total_weight /2)
 
 # create function to calculate total weight of both bots 
-def sum_weights(weight_1, weight_2):
-    return total_weight
-
-def calc_avg_weight(weight_1,weight_2):
-    return average_weight
-
-total_weight = (weight_1 + weight_2)
-average_weight = (total_weight /2)
-
-sum = total_weight
-average = average_weight
+def sum_weights():
+    sum_1 = (weight_1 + weight_2)
+    
+def cal_avg_weight():
+    average = sum_1 //2
 
 def run():
-    print("What is the weight of Beep?\n", weight_1)
-    print("What is the weight of Bop?\n", weight_2)
-    response=(input("Would you like to calculate (sum or average) "))
-    if sum:
-         print("The sum of Beep and Bop's weight is", total_weight)
-    else: 
-        if average:
-            print("The average of Beep and Bop's weight is ", average_weight)
+     print("What is the weight of Beep?")
+     weight_1 = int(input())
+     print("What is the weight of Bop?")
+     weight_2 = int(input())
+     print("What would you like to calculate (sum or average?)")
+     response =input()
+     if response =="sum":
+         print("The sum of Beep and Bop is", sum_1) 
+         
 run()
 
 
