6a052d58b0f94f28cc5bbaa5b8626e7aefcf2dbf
4josew16 <4josew16@solent.ac.uk>
2019-10-21 18:07:37 +0000

    Added code to deomonstrate the use of a simple loop.

---
+++ 1-basics/4-repetition/2-for loop/1-simple/bot.py
@@ -0,0 +1,16 @@
+#Ask user for number of mountains 
+print("How many mountains should I display?")
+mountain = int(input())
+
+# Display mountains 
+print ("\nDisplayin...")
+
+for mountain in range(mountain):
+    print("""
+           ___
+          /  \\_  
+         /^    \\
+        /  ^    \\_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+     """)
