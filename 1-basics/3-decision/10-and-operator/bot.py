dd14c6717d64fd711c426f4f7b43750ca450f219
4josew16 <4josew16@solent.ac.uk>
2019-10-21 07:40:33 +0000

    Added code to determine the use of a logical and operator.

--- 1-basics/3-decision/10-and-operator/bot.py
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -1,7 +1,9 @@
+# Ask use for what they saw or heard
 print("Did you hear a grrr or a hoot?")
 sound = input()
 print("Did you see  two red eyes or some big eyes?")
 sight = input()
+# Determine what message to display
 if  (sound == "grrr") and (sight == "two red eyes" ):
     print("There is a scary creature. I should get out here!")
 else:
