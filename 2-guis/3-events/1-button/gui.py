dd1e4ad9db824ff57fa46464034b1a1132d5b7f1
Wendy Josephs <4josew16@solent.ac.uk>
2019-11-25 17:35:58 +0100

    "Added code to display a gui using the grid layout manager."

---
+++ 2-guis/3-events/1-button/gui.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+class Gui(Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        # set window properties
+        self.title("Tickets")
+        self.configure(bg="#ccc", padx=10, pady=10)
+
+        # add components
+        self.__add_outer_frame()
+        self.__add_heading_label()
+        self.__add_instruction_label()
+        self.__add_ticket_entry()
+        self.__add_buy_button()
+
+    def __add_outer_frame(self):
+        self.outer_frame = Frame()
+        self.outer_frame.grid(row=4, column=0)
+        self.outer_frame.configure( bg="#eee", 
+                                    padx=10, 
+                                    pady=10)
+
+    def __add_heading_label(self):
+        self.heading_label = Label(self.outer_frame)
+        self.heading_label.grid(row=0, column=0,)
+        self.heading_label.configure(   bg="#eee",
+                                        font="Arial 18",
+                                        text="ENTRANCE TICKET")
+
+    def __add_instruction_label(self):
+        self.instruction_label = Label(self.outer_frame)
+        self.instruction_label.grid(row=1, column=0, sticky=W)
+        self.instruction_label.configure(   bg="#eee",
+                                            text="How many tickets are needed?")
+
+    
+
+    def __add_ticket_entry(self):
+        self.ticket_entry = Entry(self.outer_frame)
+        self.ticket_entry.grid(row=2, column=0,)
+        self.ticket_entry.configure(width=40)
+
+    def __add_buy_button(self):
+        self.buy_button = Button(self.outer_frame)
+        self.buy_button.grid(row=3,column=0,)
+        self.buy_button.configure(bg="white",
+                                        text="Buy")
