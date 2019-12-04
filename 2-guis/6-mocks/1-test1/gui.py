38989a5b400d461f6c61d2605460580c13f17f04
4josew16 <55738176+4josew16@users.noreply.github.com>
2019-12-04 21:37:18 +0000

    practice test

---
+++ 2-guis/6-mocks/1-test1/gui.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+class Gui(Tk):
+
+  # initialise window
+  def __init__(self):
+    super().__init__()
+
+# set window title 
+self.title("Newsletter")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from gui import Gui
+    
+gui = Gui()
+gui.mainloop() 
